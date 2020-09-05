d4be5a03acb4b5dd58ffb19a51f1a7e1f5352db5
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-09-06 00:04:32 +0530

    made chnges

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <StrictMode>
-    <Router>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </Router>
+    </HashRouter>
   </StrictMode>,
   document.getElementById("root")
 );
