7c3c954936003e4c7a28e9591eb07fc1b68e6226
Bhusnar5044 <bhusnarpradip@gmail.com>
2020-09-05 22:09:30 +0530

    Updates

---
+++ precache-manifest.4fe3d05b0d6e865ba7c5008fb070a7bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6ba4ef075c9a1f2dfa64e50dddec5d",
+    "url": "/pro-organiser-application/index.html"
+  },
+  {
+    "revision": "f178dab27be1c59ddbc6",
+    "url": "/pro-organiser-application/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c37bcf5e1113ab96d1f1",
+    "url": "/pro-organiser-application/static/css/main.6d8442eb.chunk.css"
+  },
+  {
+    "revision": "f178dab27be1c59ddbc6",
+    "url": "/pro-organiser-application/static/js/2.afeabf2f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/pro-organiser-application/static/js/2.afeabf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37bcf5e1113ab96d1f1",
+    "url": "/pro-organiser-application/static/js/main.0fc1f5b5.chunk.js"
+  },
+  {
+    "revision": "4d29bb84a7a4f0a75124",
+    "url": "/pro-organiser-application/static/js/runtime-main.d1569768.js"
+  },
+  {
+    "revision": "1cc1da053245ec85db2cc302a268d3bd",
+    "url": "/pro-organiser-application/static/media/stone.1cc1da05.jpg"
+  },
+  {
+    "revision": "27dd9c0ae9916d252004b7a1b10551c5",
+    "url": "/pro-organiser-application/static/media/user.27dd9c0a.png"
+  }
+]);
